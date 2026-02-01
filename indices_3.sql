ef99f751e7a246e977458125f0caef22bb5ace05
Tamayo-Goya <alejandro7tamayo@gmail.com>
2026-02-01 02:21:57 +0100

    ej3

---
+++ indices_3.sql
@@ -0,0 +1,8 @@
+--Crea un índice no único que admita nulos en la segunda para el campo
+--oficio y uno no único que no admita nulos para el campo apellido.
+
+CREATE INDEX idx_oficio 
+ON Empleados(oficio);
+
+
+
